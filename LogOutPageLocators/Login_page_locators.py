ac7bf943fa7c70b2871e2bbe5fba2c8d3896d15b
henry970 <henryokolie615@gmail.com>
2024-07-03 14:12:43 +0100

    upadate logout

--- LogOutPageLocators/Login_page_locators.py
+++ LogOutPageLocators/Login_page_locators.py
@@ -4,3 +4,7 @@ from selenium.webdriver.common.by import By
 class LogOutPageLocators:
     CLICK_PROFILE_TAB = (By.ID, "react-burger-menu-btn")
     CLICK_lOG_OUT_BUTTON = (By.ID, "logout_sidebar_link")
+
+
+class LogOutPageLocator1:
+    CLICK_HENRY = (By.ID, "")
